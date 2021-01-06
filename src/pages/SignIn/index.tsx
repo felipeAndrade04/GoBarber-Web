1c074f5f66e4dc37a6a918120570fc31738f5bed
Felipe Andrade <flpandrade2011@gmail.com>
2021-01-06 20:13:01 -0300

    Finalizando testes no login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -64,11 +64,11 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
 
-        // addToast({
-        //   type: 'error',
-        //   title: 'Erro na autenticação',
-        //   description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
-        // });
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
+        });
       }
     },
     [signIn, addToast, history],
